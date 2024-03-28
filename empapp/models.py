0aa95132ed0c925e8de959a661a4e8a76c8956f4
SANDYA S <113702289+SandyaSatheesha@users.noreply.github.com>
2024-03-28 12:43:12 +0530

    emp app

--- empapp/models.py
+++ empapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Employee(models.Model):
     empid = models.IntegerField()
--- empapp/urls.py
+++ empapp/urls.py
@@ -1,19 +1,8 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
-    path('employee/', views.create_employee),
-    path('list_emp/', views.list_employees),
-    path('delete_emp/', views.delete_emp),
-    path('ems/', views.ems),
-    path('update_employee/<pk>/', views.update_employee, name='update_employee'),
-    path('delete_employee/<pk>/', views.delete_employee, name='delete_employee'),
-    # http://localhost:8000/delete_employee/11/
-
-    # http://127.0.0.1:8000/empapp/get_employee/1234
-    path('get_employee/<pk>/', views.get_employee_by_id, name='get_employee'),
+    path("create_emp/", views.create_employee, name="create_emp"),
+    path("ems/", views.ems_home, name="ems"),
+    path("update_emp/<pk>/", views.update_employee, name='update_emp'),
+    path("delete_emp/<pk>/", views.delete_employee, name='delete_emp'),
 ]
-
-
-
-
