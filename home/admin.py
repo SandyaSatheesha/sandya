647a6339b04d2ce1d6e53b8f34457a3c5fab464c
SANDYA S <113702289+SandyaSatheesha@users.noreply.github.com>
2024-03-28 12:41:56 +0530

    pramata

---
+++ home/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+admin.site.register(MovieCategory)
+admin.site.register(Movie)
+admin.site.register(Cart)
+admin.site.register(CartItems)
+
+
